8f402d6519372c0798f45c885505d0129624fd18
Peaches_MLG <PeachesMLG100@gmail.com>
2022-06-02 21:22:15 +0100

    Added BlockPlaceListenerTest and BlockBreakListenerTest

---
+++ src/test/java/com/iridium/iridiumteams/listeners/BlockBreakListenerTest.java
@@ -0,0 +1,64 @@
+package com.iridium.iridiumteams.listeners;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.entity.PlayerMock;
+import com.iridium.iridiumcore.utils.StringUtils;
+import com.iridium.iridiumteams.PermissionType;
+import com.iridium.iridiumteams.TeamBuilder;
+import com.iridium.iridiumteams.UserBuilder;
+import com.iridium.testplugin.TestPlugin;
+import com.iridium.testplugin.TestTeam;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BlockBreakListenerTest {
+
+    private ServerMock serverMock;
+
+    @BeforeEach
+    public void setup() {
+        this.serverMock = MockBukkit.mock();
+        MockBukkit.load(TestPlugin.class);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        MockBukkit.unmock();
+    }
+
+    @Test
+    public void onBlockBreakNotCancelled__NotInTerritory() {
+        PlayerMock playerMock = new UserBuilder(serverMock).build();
+
+        assertFalse(playerMock.simulateBlockBreak(playerMock.getLocation().getBlock()).isCancelled());
+    }
+
+    @Test
+    public void onBlockBreakNotCancelled__HasPermissions() {
+        TestTeam team = new TeamBuilder().withPermission(1, PermissionType.BLOCK_BREAK, true).build();
+        PlayerMock playerMock = new UserBuilder(serverMock).withTeam(team).build();
+
+        TestPlugin.getInstance().getTeamManager().setTeamClaim(team, playerMock.getLocation());
+
+        assertFalse(playerMock.simulateBlockBreak(playerMock.getLocation().getBlock()).isCancelled());
+    }
+
+    @Test
+    public void onBlockBreakCancelled__NoPermissions() {
+        TestTeam team = new TeamBuilder().withPermission(1, PermissionType.BLOCK_BREAK, false).build();
+        PlayerMock playerMock = new UserBuilder(serverMock).withTeam(team).build();
+
+        TestPlugin.getInstance().getTeamManager().setTeamClaim(team, playerMock.getLocation());
+
+        assertTrue(playerMock.simulateBlockBreak(playerMock.getLocation().getBlock()).isCancelled());
+        playerMock.assertSaid(StringUtils.color(TestPlugin.getInstance().getMessages().cannotBreakBlocks
+                .replace("%prefix%", TestPlugin.getInstance().getConfiguration().prefix)
+        ));
+        playerMock.assertNoMoreSaid();
+    }
+}
---
+++ src/test/java/com/iridium/iridiumteams/listeners/BlockPlaceListenerTest.java
@@ -0,0 +1,65 @@
+package com.iridium.iridiumteams.listeners;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.entity.PlayerMock;
+import com.iridium.iridiumcore.utils.StringUtils;
+import com.iridium.iridiumteams.PermissionType;
+import com.iridium.iridiumteams.TeamBuilder;
+import com.iridium.iridiumteams.UserBuilder;
+import com.iridium.testplugin.TestPlugin;
+import com.iridium.testplugin.TestTeam;
+import org.bukkit.Material;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BlockPlaceListenerTest {
+
+    private ServerMock serverMock;
+
+    @BeforeEach
+    public void setup() {
+        this.serverMock = MockBukkit.mock();
+        MockBukkit.load(TestPlugin.class);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        MockBukkit.unmock();
+    }
+
+    @Test
+    public void onBlockPlaceNotCancelled__NotInTerritory() {
+        PlayerMock playerMock = new UserBuilder(serverMock).build();
+
+        assertFalse(playerMock.simulateBlockPlace(Material.STONE, playerMock.getLocation()).isCancelled());
+    }
+
+    @Test
+    public void onBlockPlaceNotCancelled__HasPermissions() {
+        TestTeam team = new TeamBuilder().withPermission(1, PermissionType.BLOCK_PLACE, true).build();
+        PlayerMock playerMock = new UserBuilder(serverMock).withTeam(team).build();
+
+        TestPlugin.getInstance().getTeamManager().setTeamClaim(team, playerMock.getLocation());
+
+        assertFalse(playerMock.simulateBlockPlace(Material.STONE, playerMock.getLocation()).isCancelled());
+    }
+
+    @Test
+    public void onBlockPlaceCancelled__NoPermissions() {
+        TestTeam team = new TeamBuilder().withPermission(1, PermissionType.BLOCK_PLACE, false).build();
+        PlayerMock playerMock = new UserBuilder(serverMock).withTeam(team).build();
+
+        TestPlugin.getInstance().getTeamManager().setTeamClaim(team, playerMock.getLocation());
+
+        assertTrue(playerMock.simulateBlockPlace(Material.STONE, playerMock.getLocation()).isCancelled());
+        playerMock.assertSaid(StringUtils.color(TestPlugin.getInstance().getMessages().cannotPlaceBlocks
+                .replace("%prefix%", TestPlugin.getInstance().getConfiguration().prefix)
+        ));
+        playerMock.assertNoMoreSaid();
+    }
+}
