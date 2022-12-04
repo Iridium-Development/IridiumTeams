a50f3e6bd512908f744e601f34ce81417c06b278
Peaches_MLG <PeachesMLG100@gmail.com>
2022-12-04 18:47:05 +0000

    Finished GUI for Settings

--- src/main/java/com/iridium/iridiumteams/Setting.java
+++ src/main/java/com/iridium/iridiumteams/Setting.java
@@ -2,21 +2,27 @@ package com.iridium.iridiumteams;
 
 import com.iridium.iridiumcore.Item;
 import com.iridium.iridiumcore.dependencies.fasterxml.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Setting {
     private Item item;
     private String displayName;
     private String defaultValue;
     @JsonIgnore
     private List<String> values;
+
+    public Setting(Item item, String displayName, String defaultValue) {
+        this.item = item;
+        this.displayName = displayName;
+        this.defaultValue = defaultValue;
+        this.values = new ArrayList<>();
+    }
 }
--- src/main/java/com/iridium/iridiumteams/configs/Settings.java
+++ src/main/java/com/iridium/iridiumteams/configs/Settings.java
@@ -9,13 +9,31 @@ import java.util.Arrays;
 public class Settings {
 
     public Setting teamJoining;
+    public Setting teamValue;
+    public Setting mobSpawning;
+    public Setting leafDecay;
+    public Setting iceForm;
+    public Setting fireSpread;
+    public Setting cropTrample;
+    public Setting weather;
+    public Setting time;
+    public Setting endermanGrief;
 
     public Settings() {
         this("Team", "&c");
     }
 
     public Settings(String team, String color) {
-        teamJoining = new Setting(new Item(XMaterial.COBBLESTONE, 10, 1, color + team + " Type", Arrays.asList("&7Set your " + team + " joining method.", "", "" + color + "&lValue", "&7%value%")), "JoinType", "Private", Arrays.asList("Private", "Public"));
+        teamJoining = new Setting(new Item(XMaterial.COBBLESTONE, 10, 1, color + team + " Type", Arrays.asList("&7Set your " + team + " joining method.", "", "" + color + "&lValue", "&7%value%")), "JoinType", "Private");
+        teamValue = new Setting(new Item(XMaterial.SUNFLOWER, 11, 1, color + team + " Value Visibility", Arrays.asList("&7Set your " + team + " value.", "", "" + color + "&lValue", "&7%value%")), "ValueVisibility", "Public");
+        mobSpawning = new Setting(new Item(XMaterial.SPAWNER, 12, 1, color + team + " Mob Spawning", Arrays.asList("&7Control Mob Spawning on your " + team + ".", "", "" + color + "&lValue", "&7%value%")), "MobSpawning", "Enabled");
+        leafDecay = new Setting(new Item(XMaterial.JUNGLE_LEAVES, 13, 1, color + team + " Leaf Decay", Arrays.asList("&7Control Leaf Decay on your " + team + ".", "", "" + color + "&lValue", "&7%value%")), "LeafDecay", "Disabled");
+        iceForm = new Setting(new Item(XMaterial.ICE, 14, 1, color + team + " Ice Form", Arrays.asList("&7Control Ice Forming on your " + team + ".", "", "" + color + "&lValue", "&7%value%")), "IceForm", "Disabled");
+        fireSpread = new Setting(new Item(XMaterial.FLINT_AND_STEEL, 15, 1, color + team + " Fire Spread", Arrays.asList("&7Control Fire Spread on your " + team + ".", "", "" + color + "&lValue", "&7%value%")), "FireSpread", "Disabled");
+        cropTrample = new Setting(new Item(XMaterial.WHEAT_SEEDS, 16, 1, color + team + " Crop Trample", Arrays.asList("&7Control Trampling Crops on your " + team + ".", "", "" + color + "&lValue", "&7%value%")), "CropTrample", "Enabled");
+        weather = new Setting(new Item(XMaterial.COBBLESTONE, 19, 1, color + team + " Weather", Arrays.asList("&7Control Weather on your " + team + ".", "", "" + color + "&lValue", "&7%value%")), "Weather", "Server");
+        time = new Setting(new Item(XMaterial.CLOCK, 20, 1, color + team + " Time", Arrays.asList("&7Control Time on your " + team + ".", "", "" + color + "&lValue", "&7%value%")), "Time", "Server");
+        endermanGrief = new Setting(new Item(XMaterial.COBBLESTONE, 21, 1, color + team + " Enderman Grief", Arrays.asList("&7Control Enderman Grief on your " + team + ".", "", "" + color + "&lValue", "&7%value%")), "EndermanGrief", "Disabled");
     }
 
 }
