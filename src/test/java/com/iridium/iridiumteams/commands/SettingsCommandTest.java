2135a889662774801e7cb5a4c65d7bc7bbf5aa34
Peaches_MLG <PeachesMLG100@gmail.com>
2022-12-04 19:05:43 +0000

    Added Unit Tests

---
+++ src/test/java/com/iridium/iridiumteams/commands/SettingsCommandTest.java
@@ -0,0 +1,119 @@
+package com.iridium.iridiumteams.commands;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.entity.PlayerMock;
+import com.iridium.iridiumcore.utils.StringUtils;
+import com.iridium.iridiumteams.SettingType;
+import com.iridium.iridiumteams.TeamBuilder;
+import com.iridium.iridiumteams.UserBuilder;
+import com.iridium.iridiumteams.gui.SettingsGUI;
+import com.iridium.testplugin.TestPlugin;
+import com.iridium.testplugin.TestTeam;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SettingsCommandTest {
+
+    private ServerMock serverMock;
+
+    @BeforeEach
+    public void setup() {
+        this.serverMock = MockBukkit.mock();
+        MockBukkit.load(TestPlugin.class);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        MockBukkit.unmock();
+    }
+
+    @Test
+    public void executeSettingsCommand__InvalidSyntax() {
+        TestTeam testTeam = new TeamBuilder().build();
+        PlayerMock playerMock = new UserBuilder(serverMock).withTeam(testTeam).build();
+        serverMock.dispatchCommand(playerMock, "test settings invalid");
+        playerMock.assertSaid(StringUtils.color(TestPlugin.getInstance().getCommands().settingsCommand.syntax
+                .replace("%prefix%", TestPlugin.getInstance().getConfiguration().prefix)
+        ));
+        playerMock.assertNoMoreSaid();
+    }
+
+    @Test
+    public void executeSettingsCommand__NoTeam() {
+        PlayerMock playerMock = new UserBuilder(serverMock).build();
+        serverMock.dispatchCommand(playerMock, "test settings");
+        playerMock.assertSaid(StringUtils.color(TestPlugin.getInstance().getMessages().dontHaveTeam
+                .replace("%prefix%", TestPlugin.getInstance().getConfiguration().prefix)
+        ));
+        playerMock.assertNoMoreSaid();
+    }
+
+    @Test
+    public void executeSettingsCommand__OpensGUI() {
+        TestTeam testTeam = new TeamBuilder().build();
+        PlayerMock playerMock = new UserBuilder(serverMock).withTeam(testTeam).build();
+        serverMock.dispatchCommand(playerMock, "test settings");
+        assertTrue(playerMock.getOpenInventory().getTopInventory().getHolder() instanceof SettingsGUI<?, ?>);
+    }
+
+    @Test
+    public void executeSettingsCommand__UnknownSetting() {
+        TestTeam testTeam = new TeamBuilder().build();
+        PlayerMock playerMock = new UserBuilder(serverMock).withTeam(testTeam).build();
+        serverMock.dispatchCommand(playerMock, "test settings unknown Enabled");
+        playerMock.assertSaid(StringUtils.color(TestPlugin.getInstance().getMessages().invalidSetting
+                .replace("%prefix%", TestPlugin.getInstance().getConfiguration().prefix)
+        ));
+        playerMock.assertNoMoreSaid();
+    }
+
+    @Test
+    public void executeSettingsCommand__UnknownSettingValue() {
+        TestTeam testTeam = new TeamBuilder().build();
+        PlayerMock playerMock = new UserBuilder(serverMock).withTeam(testTeam).build();
+        serverMock.dispatchCommand(playerMock, "test settings weather unknown");
+        playerMock.assertSaid(StringUtils.color(TestPlugin.getInstance().getMessages().invalidSettingValue
+                .replace("%prefix%", TestPlugin.getInstance().getConfiguration().prefix)
+        ));
+        playerMock.assertNoMoreSaid();
+    }
+
+    @Test
+    public void executeSettingsCommand__SettingSet() {
+        TestTeam testTeam = new TeamBuilder().build();
+        PlayerMock playerMock = new UserBuilder(serverMock).withTeam(testTeam).build();
+        serverMock.dispatchCommand(playerMock, "test settings time day");
+        playerMock.assertSaid(StringUtils.color(TestPlugin.getInstance().getMessages().settingSet
+                .replace("%prefix%", TestPlugin.getInstance().getConfiguration().prefix)
+                .replace("%setting%", "Time")
+                .replace("%value%", "Day")
+        ));
+        playerMock.assertNoMoreSaid();
+        assertEquals("Day", TestPlugin.getInstance().getTeamManager().getTeamSetting(testTeam, SettingType.TIME.getSettingKey()).getValue());
+    }
+
+    @Test
+    public void settingsCommand__TabComplete() {
+        PlayerMock playerMock = new UserBuilder(serverMock).build();
+
+        assertEquals(Arrays.asList("CropTrample", "EndermanGrief", "FireSpread", "IceForm", "JoinType", "LeafDecay", "MobSpawning", "Time", "ValueVisibility", "Weather"), serverMock.getCommandTabComplete(playerMock, "test settings "));
+
+        assertEquals(Arrays.asList("Disabled", "Enabled"), serverMock.getCommandTabComplete(playerMock, "test settings IceForm "));
+
+        assertEquals(Arrays.asList("Raining", "Server", "Sunny", "Thunder"), serverMock.getCommandTabComplete(playerMock, "test settings Weather "));
+
+        assertEquals(Arrays.asList("Day", "Midnight", "Morning", "Night", "Noon", "Server", "Sunrise", "Sunset"), serverMock.getCommandTabComplete(playerMock, "test settings time "));
+
+        assertEquals(Arrays.asList("Private", "Public"), serverMock.getCommandTabComplete(playerMock, "test settings JoinType "));
+
+        assertEquals(Collections.emptyList(), serverMock.getCommandTabComplete(playerMock, "test settings invalid "));
+    }
+}
---
+++ src/test/java/com/iridium/iridiumteams/gui/SettingsGUITest.java
@@ -0,0 +1,83 @@
+package com.iridium.iridiumteams.gui;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import be.seeseemelk.mockbukkit.entity.PlayerMock;
+import com.google.common.collect.ImmutableMap;
+import com.iridium.iridiumcore.utils.StringUtils;
+import com.iridium.iridiumteams.SettingType;
+import com.iridium.iridiumteams.TeamBuilder;
+import com.iridium.iridiumteams.UserBuilder;
+import com.iridium.testplugin.TestPlugin;
+import com.iridium.testplugin.TestTeam;
+import com.iridium.testplugin.User;
+import org.bukkit.Material;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SettingsGUITest {
+
+    private ServerMock serverMock;
+
+    private final Map<Integer, Material> inventoryLayout = new ImmutableMap.Builder<Integer, Material>()
+            .put(10, Material.COBBLESTONE)
+            .put(11, Material.SUNFLOWER)
+            .put(12, Material.SPAWNER)
+            .put(13, Material.JUNGLE_LEAVES)
+            .put(14, Material.ICE)
+            .put(15, Material.FLINT_AND_STEEL)
+            .put(16, Material.WHEAT_SEEDS)
+            .put(19, Material.COBBLESTONE)
+            .put(20, Material.CLOCK)
+            .put(21, Material.COBBLESTONE)
+            .build();
+
+    @BeforeEach
+    public void setup() {
+        this.serverMock = MockBukkit.mock();
+        MockBukkit.load(TestPlugin.class);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        MockBukkit.unmock();
+    }
+
+    @Test
+    public void SettingsGUI__HasItems() {
+        TestTeam testTeam = new TeamBuilder().build();
+        SettingsGUI<TestTeam, User> settingsGUI = new SettingsGUI<>(testTeam, null, TestPlugin.getInstance());
+        Inventory inventory = settingsGUI.getInventory();
+        for (int i = 0; i < inventory.getSize(); i++) {
+            assertEquals(inventoryLayout.getOrDefault(i, Material.BLACK_STAINED_GLASS_PANE), inventory.getContents()[i].getType(), "Item on slot " + i + " not as expected");
+        }
+    }
+
+    @Test
+    public void SettingsGUI__OnClick() {
+        TestTeam testTeam = new TeamBuilder().build();
+        PlayerMock playerMock = new UserBuilder(serverMock).withTeam(testTeam).build();
+        SettingsGUI<TestTeam, User> settingsGUI = new SettingsGUI<>(testTeam, null, TestPlugin.getInstance());
+        Inventory inventory = settingsGUI.getInventory();
+
+        playerMock.openInventory(inventory);
+        InventoryClickEvent inventoryClickEvent = playerMock.simulateInventoryClick(10);
+
+        assertTrue(inventoryClickEvent.isCancelled());
+        playerMock.assertSaid(StringUtils.color(TestPlugin.getInstance().getMessages().settingSet
+                .replace("%prefix%", TestPlugin.getInstance().getConfiguration().prefix)
+                .replace("%setting%", "JoinType")
+                .replace("%value%", "Public")
+        ));
+        playerMock.assertNoMoreSaid();
+        assertEquals("Public", TestPlugin.getInstance().getTeamManager().getTeamSetting(testTeam, SettingType.TEAM_TYPE.getSettingKey()).getValue());
+    }
+}
