421ca34b0022341762d6de3e1e895ed213ca2b04
Peaches_MLG <PeachesMLG100@gmail.com>
2022-12-05 14:08:15 +0000

    Implemented Team Value setting

--- src/test/java/com/iridium/iridiumteams/gui/TopGUITest.java
+++ src/test/java/com/iridium/iridiumteams/gui/TopGUITest.java
@@ -4,6 +4,7 @@ import be.seeseemelk.mockbukkit.MockBukkit;
 import be.seeseemelk.mockbukkit.ServerMock;
 import be.seeseemelk.mockbukkit.entity.PlayerMock;
 import com.google.common.collect.ImmutableMap;
+import com.iridium.iridiumteams.SettingType;
 import com.iridium.iridiumteams.TeamBuilder;
 import com.iridium.iridiumteams.UserBuilder;
 import com.iridium.iridiumteams.sorting.ExperienceTeamSort;
@@ -24,10 +25,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TopGUITest {
-
-
     private final Map<Integer, Material> inventoryLayout = new ImmutableMap.Builder<Integer, Material>()
             .put(14, Material.PLAYER_HEAD)
+            .put(22, Material.BARRIER)
+            .put(23, Material.BARRIER)
+            .put(24, Material.BARRIER)
+            .put(30, Material.BARRIER)
+            .put(31, Material.BARRIER)
+            .put(32, Material.BARRIER)
+            .put(33, Material.BARRIER)
+            .put(34, Material.BARRIER)
+            .put(18, Material.DIAMOND)
+            .put(27, Material.EXPERIENCE_BOTTLE)
+            .put(51, Material.LIME_STAINED_GLASS_PANE)
+            .put(47, Material.RED_STAINED_GLASS_PANE)
+            .build();
+    private final Map<Integer, Material> noPlayersInventoryLayout = new ImmutableMap.Builder<Integer, Material>()
+            .put(14, Material.BARRIER)
             .put(22, Material.BARRIER)
             .put(23, Material.BARRIER)
             .put(24, Material.BARRIER)
@@ -61,6 +75,16 @@ class TopGUITest {
         Inventory inventory = topGUI.getInventory();
         for (int i = 0; i < inventory.getSize(); i++) {
             assertEquals(inventoryLayout.getOrDefault(i, Material.BLACK_STAINED_GLASS_PANE), inventory.getContents()[i].getType(), "Item on slot " + i + " not as expected");
+        }
+    }
+
+    @Test
+    public void TopGUI__HidesPrivateValues() {
+        new TeamBuilder().withSetting(SettingType.VALUE_VISIBILITY, "Private").build();
+        TopGUI<TestTeam, User> topGUI = new TopGUI<>(TestPlugin.getInstance().getTop().valueTeamSort, null, TestPlugin.getInstance());
+        Inventory inventory = topGUI.getInventory();
+        for (int i = 0; i < inventory.getSize(); i++) {
+            assertEquals(noPlayersInventoryLayout.getOrDefault(i, Material.BLACK_STAINED_GLASS_PANE), inventory.getContents()[i].getType(), "Item on slot " + i + " not as expected");
         }
     }
 
