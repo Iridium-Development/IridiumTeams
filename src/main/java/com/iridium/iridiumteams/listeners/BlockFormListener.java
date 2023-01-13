f00ad10edfa57706c2612100219563b4b3bc4dea
Peaches_MLG <PeachesMLG100@gmail.com>
2023-01-13 22:49:01 +0000

    Implemented Leaf Decay, IceForm and Fire Spread

---
+++ src/main/java/com/iridium/iridiumteams/listeners/BlockFormListener.java
@@ -0,0 +1,28 @@
+package com.iridium.iridiumteams.listeners;
+
+import com.iridium.iridiumteams.IridiumTeams;
+import com.iridium.iridiumteams.SettingType;
+import com.iridium.iridiumteams.database.IridiumUser;
+import com.iridium.iridiumteams.database.Team;
+import com.iridium.iridiumteams.database.TeamSetting;
+import lombok.AllArgsConstructor;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFormEvent;
+
+@AllArgsConstructor
+public class BlockFormListener<T extends Team, U extends IridiumUser<T>> implements Listener {
+    private final IridiumTeams<T, U> iridiumTeams;
+
+    @EventHandler(ignoreCancelled = true)
+    public void onBlockForm(BlockFormEvent event) {
+        iridiumTeams.getTeamManager().getTeamViaLocation(event.getBlock().getLocation()).ifPresent(team -> {
+            TeamSetting teamType = iridiumTeams.getTeamManager().getTeamSetting(team, SettingType.ICE_FORM.getSettingKey());
+            if (teamType.getValue().equalsIgnoreCase("Disabled") && (event.getNewState().getType() == Material.ICE || event.getNewState().getType() == Material.SNOW)) {
+                event.setCancelled(true);
+            }
+        });
+    }
+
+}
