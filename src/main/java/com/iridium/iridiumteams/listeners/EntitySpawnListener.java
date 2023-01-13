9f6a589113d95e1582ec1e99ac02bfa4dcfcc733
Peaches_MLG <PeachesMLG100@gmail.com>
2023-01-13 21:31:05 +0000

    Implemented MobSpawning setting type

--- src/main/java/com/iridium/iridiumteams/listeners/EntitySpawnListener.java
+++ src/main/java/com/iridium/iridiumteams/listeners/EntitySpawnListener.java
@@ -2,9 +2,13 @@
 package com.iridium.iridiumteams.listeners;
 
 import com.iridium.iridiumteams.IridiumTeams;
+import com.iridium.iridiumteams.SettingType;
 import com.iridium.iridiumteams.database.IridiumUser;
 import com.iridium.iridiumteams.database.Team;
+import com.iridium.iridiumteams.database.TeamSetting;
 import lombok.AllArgsConstructor;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
@@ -19,6 +23,12 @@ public class EntitySpawnListener<T extends Team, U extends IridiumUser<T>> imple
     @EventHandler(ignoreCancelled = true)
     public void onEntitySpawn(EntitySpawnEvent event) {
         Optional<T> currentTeam = iridiumTeams.getTeamManager().getTeamViaLocation(event.getEntity().getLocation());
+        if (currentTeam.isPresent()) {
+            TeamSetting teamType = iridiumTeams.getTeamManager().getTeamSetting(currentTeam.get(), SettingType.MOB_SPAWNING.getSettingKey());
+            if (teamType.getValue().equalsIgnoreCase("Disabled") && event.getEntity() instanceof LivingEntity && event.getEntityType() != EntityType.ARMOR_STAND) {
+                event.setCancelled(true);
+            }
+        }
 
         event.getEntity().setMetadata("team_spawned", new FixedMetadataValue(iridiumTeams, currentTeam.map(T::getId).orElse(0)));
     }
