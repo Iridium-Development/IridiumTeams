62e041adddc43ab55fe96653dea670740d7045a5
Peaches_MLG <PeachesMLG100@gmail.com>
2023-01-14 14:00:40 +0000

    Added Entity Grief setting

---
+++ src/main/java/com/iridium/iridiumteams/listeners/EntityChangeBlockListener.java
@@ -0,0 +1,39 @@
+
+package com.iridium.iridiumteams.listeners;
+
+import com.iridium.iridiumteams.IridiumTeams;
+import com.iridium.iridiumteams.SettingType;
+import com.iridium.iridiumteams.database.IridiumUser;
+import com.iridium.iridiumteams.database.Team;
+import com.iridium.iridiumteams.database.TeamSetting;
+import lombok.AllArgsConstructor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityBreakDoorEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+
+@AllArgsConstructor
+public class EntityChangeBlockListener<T extends Team, U extends IridiumUser<T>> implements Listener {
+    private final IridiumTeams<T, U> iridiumTeams;
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+        iridiumTeams.getTeamManager().getTeamViaLocation(event.getBlock().getLocation()).ifPresent(team -> {
+            TeamSetting teamType = iridiumTeams.getTeamManager().getTeamSetting(team, SettingType.ENTITY_GRIEF.getSettingKey());
+            if (teamType.getValue().equalsIgnoreCase("Disabled")) {
+                event.setCancelled(true);
+            }
+        });
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityBreakDoor(EntityBreakDoorEvent event) {
+        iridiumTeams.getTeamManager().getTeamViaLocation(event.getBlock().getLocation()).ifPresent(team -> {
+            TeamSetting teamType = iridiumTeams.getTeamManager().getTeamSetting(team, SettingType.ENTITY_GRIEF.getSettingKey());
+            if (teamType.getValue().equalsIgnoreCase("Disabled")) {
+                event.setCancelled(true);
+            }
+        });
+    }
+
+}
