56271ae1f785194b8dad05427412e4c4859117e3
Peaches_MLG <PeachesMLG100@gmail.com>
2023-01-14 13:58:10 +0000

    Added Crop Trampling

---
+++ src/main/java/com/iridium/iridiumteams/listeners/EntityInteractListener.java
@@ -0,0 +1,42 @@
+
+package com.iridium.iridiumteams.listeners;
+
+import com.iridium.iridiumcore.dependencies.xseries.XMaterial;
+import com.iridium.iridiumteams.IridiumTeams;
+import com.iridium.iridiumteams.SettingType;
+import com.iridium.iridiumteams.database.IridiumUser;
+import com.iridium.iridiumteams.database.Team;
+import com.iridium.iridiumteams.database.TeamSetting;
+import lombok.AllArgsConstructor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+@AllArgsConstructor
+public class EntityInteractListener<T extends Team, U extends IridiumUser<T>> implements Listener {
+    private final IridiumTeams<T, U> iridiumTeams;
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityInteract(EntityInteractEvent event) {
+        iridiumTeams.getTeamManager().getTeamViaLocation(event.getBlock().getLocation()).ifPresent(team -> {
+            TeamSetting teamType = iridiumTeams.getTeamManager().getTeamSetting(team, SettingType.CROP_TRAMPLE.getSettingKey());
+            if (teamType.getValue().equalsIgnoreCase("Disabled") && (XMaterial.matchXMaterial(event.getBlock().getType()) == XMaterial.FARMLAND)) {
+                event.setCancelled(true);
+            }
+        });
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getAction() != Action.PHYSICAL || event.getClickedBlock() == null) return;
+        iridiumTeams.getTeamManager().getTeamViaLocation(event.getClickedBlock().getLocation()).ifPresent(team -> {
+            TeamSetting teamType = iridiumTeams.getTeamManager().getTeamSetting(team, SettingType.CROP_TRAMPLE.getSettingKey());
+            if (teamType.getValue().equalsIgnoreCase("Disabled") && (XMaterial.matchXMaterial(event.getClickedBlock().getType()) == XMaterial.FARMLAND)) {
+                event.setCancelled(true);
+            }
+        });
+    }
+
+}
