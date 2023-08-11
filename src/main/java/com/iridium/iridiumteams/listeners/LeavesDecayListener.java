5e2a01041f8940ecf9fb110424f6eb337fd9bee8
Shyanne <randomgirlisweird@gmail.com>
2023-08-11 09:42:14 -0400

    Update LeavesDecayListener.java

--- src/main/java/com/iridium/iridiumteams/listeners/LeavesDecayListener.java
+++ src/main/java/com/iridium/iridiumteams/listeners/LeavesDecayListener.java
@@ -7,6 +7,7 @@ import com.iridium.iridiumteams.database.Team;
 import com.iridium.iridiumteams.database.TeamSetting;
 import lombok.AllArgsConstructor;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.LeavesDecayEvent;
 
@@ -14,7 +15,7 @@ import org.bukkit.event.block.LeavesDecayEvent;
 public class LeavesDecayListener<T extends Team, U extends IridiumUser<T>> implements Listener {
     private final IridiumTeams<T, U> iridiumTeams;
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
     public void onLeavesDecay(LeavesDecayEvent event) {
         iridiumTeams.getTeamManager().getTeamViaLocation(event.getBlock().getLocation()).ifPresent(team -> {
             TeamSetting teamSetting = iridiumTeams.getTeamManager().getTeamSetting(team, SettingType.LEAF_DECAY.getSettingKey());
