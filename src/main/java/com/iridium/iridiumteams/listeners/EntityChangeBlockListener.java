fc9d7ea11a2a8dbf3587728fdd3cc43bf9e90d0e
Shyanne <randomgirlisweird@gmail.com>
2023-08-11 08:45:08 -0400

    Update EntityChangeBlockListener.java
    see https://github.com/Iridium-Development/IridiumSkyblock/issues/702

--- src/main/java/com/iridium/iridiumteams/listeners/EntityChangeBlockListener.java
+++ src/main/java/com/iridium/iridiumteams/listeners/EntityChangeBlockListener.java
@@ -7,6 +7,7 @@ import com.iridium.iridiumteams.database.IridiumUser;
 import com.iridium.iridiumteams.database.Team;
 import com.iridium.iridiumteams.database.TeamSetting;
 import lombok.AllArgsConstructor;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityBreakDoorEvent;
@@ -20,6 +21,11 @@ public class EntityChangeBlockListener<T extends Team, U extends IridiumUser<T>>
     public void onEntityChangeBlock(EntityChangeBlockEvent event) {
         iridiumTeams.getTeamManager().getTeamViaLocation(event.getBlock().getLocation()).ifPresent(team -> {
             TeamSetting teamSetting = iridiumTeams.getTeamManager().getTeamSetting(team, SettingType.ENTITY_GRIEF.getSettingKey());
+
+            if(event.getEntity() instanceof FallingBlock) {
+                return;
+            }
+
             if (teamSetting.getValue().equalsIgnoreCase("Disabled")) {
                 event.setCancelled(true);
             }
