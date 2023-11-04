6dead8bad6bee38a3e794f29eae6c27fa470d746
SlashRemix <69310169+MertUnverdi@users.noreply.github.com>
2023-11-04 13:07:18 +0300

    Update TeamTransferEvent.java
    other fields shouldn't be manipulated

--- src/main/java/com/iridium/iridiumteams/api/TeamTransferEvent.java
+++ src/main/java/com/iridium/iridiumteams/api/TeamTransferEvent.java
@@ -10,20 +10,19 @@ import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
 @Getter
-@Setter
 public class TeamTransferEvent<T extends Team, U extends IridiumUser<T>, U2 extends IridiumUser<T>> extends Event implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
     private final T team;
     private final U oldUser;
     private final U2 newUser;
+    @Setter
     private boolean cancelled;
 
     public TeamTransferEvent(final T team, U oldUser, U2 newUser) {
         this.team = team;
         this.oldUser = oldUser;
         this.newUser = newUser;
-        this.cancelled = false;
     }
 
     public @NotNull HandlerList getHandlers() {
