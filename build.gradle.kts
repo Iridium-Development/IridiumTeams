2e4b4a3fa6dafb239dae3bd652329d950edcf9c3
Peaches_MLG <PeachesMLG100@gmail.com>
2024-03-20 10:34:29 +0000

    Version Increment

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.iridium"
-version = "2.4.1"
+version = "2.4.2"
 description = "IridiumTeams"
 
 repositories {
@@ -24,7 +24,7 @@ dependencies {
     implementation("org.jetbrains:annotations:24.1.0")
     implementation("com.j256.ormlite:ormlite-core:6.1")
     implementation("com.j256.ormlite:ormlite-jdbc:6.1")
-    implementation("com.iridium:IridiumCore:1.8.7")
+    implementation("com.iridium:IridiumCore:1.8.8")
 
     // Other dependencies that are not required or already available at runtime
     compileOnly("org.projectlombok:lombok:1.18.30")
