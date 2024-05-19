545bc809f6557e195d72cd47fbf24838e04840d0
Peaches_MLG <PeachesMLG100@gmail.com>
2024-05-19 22:15:27 +0100

    Bump Core version

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,8 +29,7 @@ dependencies {
     implementation("org.jetbrains:annotations:24.1.0")
     implementation("com.j256.ormlite:ormlite-core:6.1")
     implementation("com.j256.ormlite:ormlite-jdbc:6.1")
-    //implementation("com.iridium:IridiumCore:1.9.6")
-    implementation(files("build/depend/IridiumCore-1.9.7.jar"))
+    implementation("com.iridium:IridiumCore:1.9.7-B1")
 
     // Other dependencies that are not required or already available at runtime
     compileOnly("org.projectlombok:lombok:1.18.32")
