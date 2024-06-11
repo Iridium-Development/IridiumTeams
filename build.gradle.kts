af0278ad828a1993469c09464cb0bcb65cb8721b
Peaches_MLG <PeachesMLG100@gmail.com>
2024-06-11 14:01:20 +0100

    Bump Core

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.iridium"
-version = "2.5.6"
+version = "2.5.7"
 description = "IridiumTeams"
 
 repositories {
@@ -29,7 +29,7 @@ dependencies {
     implementation("org.jetbrains:annotations:24.1.0")
     implementation("com.j256.ormlite:ormlite-core:6.1")
     implementation("com.j256.ormlite:ormlite-jdbc:6.1")
-    implementation("com.iridium:IridiumCore:2.0.4")
+    implementation("com.iridium:IridiumCore:2.0.5")
 
     // Other dependencies that are not required or already available at runtime
     compileOnly("org.projectlombok:lombok:1.18.32")
