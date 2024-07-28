0f00316fb19a05c79bb620807342ae6d54c63c02
Genre Mamanao <41022228+Faun471@users.noreply.github.com>
2024-07-28 21:43:26 +0800

    Add confirmable commands (#85)
    Co-authored-by: Peaches_MLG <PeachesMLG100@gmail.com>

--- src/main/java/com/iridium/iridiumteams/commands/DeleteCommand.java
+++ src/main/java/com/iridium/iridiumteams/commands/DeleteCommand.java
@@ -5,7 +5,6 @@ import com.iridium.iridiumteams.IridiumTeams;
 import com.iridium.iridiumteams.Rank;
 import com.iridium.iridiumteams.database.IridiumUser;
 import com.iridium.iridiumteams.database.Team;
-import com.iridium.iridiumteams.gui.ConfirmationGUI;
 import lombok.NoArgsConstructor;
 import org.bukkit.entity.Player;
 
@@ -13,11 +12,11 @@ import java.util.List;
 import java.util.Optional;
 
 @NoArgsConstructor
-public class DeleteCommand<T extends Team, U extends IridiumUser<T>> extends Command<T, U> {
+public class DeleteCommand<T extends Team, U extends IridiumUser<T>> extends ConfirmableCommand<T, U> {
     public String adminPermission;
 
-    public DeleteCommand(List<String> args, String description, String syntax, String permission, long cooldownInSeconds, String adminPermission) {
-        super(args, description, syntax, permission, cooldownInSeconds);
+    public DeleteCommand(List<String> args, String description, String syntax, String permission, long cooldownInSeconds, String adminPermission, boolean requiresConfirmation) {
+        super(args, description, syntax, permission, cooldownInSeconds, requiresConfirmation);
         this.adminPermission = adminPermission;
     }
 
@@ -31,53 +30,64 @@ public class DeleteCommand<T extends Team, U extends IridiumUser<T>> extends Com
                 ));
                 return false;
             }
+
             Optional<T> team = iridiumTeams.getTeamManager().getTeamViaNameOrPlayer(arguments[0]);
             if (!team.isPresent()) {
                 player.sendMessage(StringUtils.color(iridiumTeams.getMessages().teamDoesntExistByName
-                        .replace("%prefix%", iridiumTeams.getConfiguration().prefix))
-                );
+                        .replace("%prefix%", iridiumTeams.getConfiguration().prefix)
+                ));
                 return false;
             }
-            deleteTeam(user, team.get(), iridiumTeams, true);
-            return true;
+            return execute(user, team.get(), arguments, iridiumTeams);
         }
         return super.execute(user, arguments, iridiumTeams);
     }
 
     @Override
-    public boolean execute(U user, T team, String[] arguments, IridiumTeams<T, U> iridiumTeams) {
+    protected boolean isCommandValid(U user, T team, String[] arguments, IridiumTeams<T, U> iridiumTeams) {
         Player player = user.getPlayer();
+        if (arguments.length == 1) {
+            return true;
+        }
+
         if (user.getUserRank() != Rank.OWNER.getId() && !user.isBypassing()) {
             player.sendMessage(StringUtils.color(iridiumTeams.getMessages().cannotDeleteTeam
                     .replace("%prefix%", iridiumTeams.getConfiguration().prefix)
             ));
             return false;
         }
-        deleteTeam(user, team, iridiumTeams, false);
         return true;
+    }
+
+    @Override
+    protected void executeAfterConfirmation(U user, T team, String[] arguments, IridiumTeams<T, U> iridiumTeams) {
+        if (arguments.length == 1) {
+            deleteTeam(user, team, iridiumTeams, true);
+        }
+
+        deleteTeam(user, team, iridiumTeams, false);
     }
 
     private void deleteTeam(U user, T team, IridiumTeams<T, U> iridiumTeams, boolean admin) {
         Player player = user.getPlayer();
-        player.openInventory(new ConfirmationGUI<>(() -> {
-            if (!iridiumTeams.getTeamManager().deleteTeam(team, user)) return;
-            for (U member : iridiumTeams.getTeamManager().getTeamMembers(team)) {
-                member.setTeamID(0);
-                Player teamMember = member.getPlayer();
-                if (teamMember != null) {
-                    teamMember.sendMessage(StringUtils.color(iridiumTeams.getMessages().teamDeleted
-                            .replace("%prefix%", iridiumTeams.getConfiguration().prefix)
-                            .replace("%player%", player.getName())
-                    ));
-                }
-            }
-            if (admin) {
-                player.sendMessage(StringUtils.color(iridiumTeams.getMessages().deletedPlayerTeam
+        if (!iridiumTeams.getTeamManager().deleteTeam(team, user)) return;
+
+        for (U member : iridiumTeams.getTeamManager().getTeamMembers(team)) {
+            member.setTeamID(0);
+            Player teamMember = member.getPlayer();
+            if (teamMember != null) {
+                teamMember.sendMessage(StringUtils.color(iridiumTeams.getMessages().teamDeleted
                         .replace("%prefix%", iridiumTeams.getConfiguration().prefix)
-                        .replace("%name%", team.getName())
+                        .replace("%player%", player.getName())
                 ));
             }
-        }, iridiumTeams).getInventory());
+        }
+        if (admin) {
+            player.sendMessage(StringUtils.color(iridiumTeams.getMessages().deletedPlayerTeam
+                    .replace("%prefix%", iridiumTeams.getConfiguration().prefix)
+                    .replace("%name%", team.getName())
+            ));
+        }
     }
 
 }
