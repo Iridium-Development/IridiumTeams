e3d917e65598441be711b64af246e2e6b275a813
Peaches_MLG <PeachesMLG100@gmail.com>
2024-12-28 13:32:53 +0000

    Version Increment

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.iridium"
-version = "2.5.9.7"
+version = "2.5.9.8"
 description = "IridiumTeams"
 
 repositories {
@@ -29,7 +29,7 @@ dependencies {
     implementation("org.jetbrains:annotations:26.0.1")
     implementation("com.j256.ormlite:ormlite-core:6.1")
     implementation("com.j256.ormlite:ormlite-jdbc:6.1")
-    implementation("com.iridium:IridiumCore:2.0.8.7")
+    implementation("com.iridium:IridiumCore:2.0.8.8")
 
     // Other dependencies that are not required or already available at runtime
     compileOnly("org.projectlombok:lombok:1.18.36")
@@ -82,7 +82,7 @@ tasks {
 
     test {
         // TODO: fix unit tests & re-enable this
-        // useJUnitPlatform()
+         useJUnitPlatform()
     }
 
     compileJava {
