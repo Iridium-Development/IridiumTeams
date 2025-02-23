190b1f35ed1ed90e726c22b0b34a42bd29e16fcc
Genre Mamanao <41022228+Faun471@users.noreply.github.com>
2025-02-23 12:52:12 +0800

    Environtmental Kill

--- src/main/java/com/iridium/iridiumteams/listeners/EntityDeathListener.java
+++ src/main/java/com/iridium/iridiumteams/listeners/EntityDeathListener.java
@@ -1,4 +1,3 @@
-
 package com.iridium.iridiumteams.listeners;
 
 import com.iridium.iridiumteams.IridiumTeams;
@@ -10,20 +9,55 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.damage.DamageSource;
+import org.bukkit.damage.DamageType;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @AllArgsConstructor
 public class EntityDeathListener<T extends Team, U extends IridiumUser<T>> implements Listener {
     private final IridiumTeams<T, U> iridiumTeams;
 
+    private static final Set<DamageType> TRACKED_DAMAGE_TYPES;
+
+    static {
+        Set<DamageType> damageTypes = new HashSet<>();
+        damageTypes.add(DamageType.FALL);
+        damageTypes.add(DamageType.LAVA);
+        damageTypes.add(DamageType.CAMPFIRE);
+        TRACKED_DAMAGE_TYPES = new HashSet<>(damageTypes);
+    }
+
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void monitorEntityDeath(EntityDeathEvent event) {
         Player killer = event.getEntity().getKiller();
-        if(killer==null)return;
-        U user = iridiumTeams.getUserManager().getUser(killer);
-        iridiumTeams.getTeamManager().getTeamViaID(user.getTeamID()).ifPresent(team -> {
-            iridiumTeams.getMissionManager().handleMissionUpdate(team, killer.getLocation().getWorld(), "KILL", event.getEntityType().name(), 1);
-        });
 
+        if (killer == null && event.getEntity().getLastDamageCause() != null) {
+            DamageSource damageSource = event.getEntity().getLastDamageCause().getDamageSource();
+            DamageType damageType = damageSource.getDamageType();
+
+            if (TRACKED_DAMAGE_TYPES.contains(damageType)) {
+                iridiumTeams.getTeamManager().getTeamViaLocation(event.getEntity().getLocation()).ifPresent(team -> {
+                    iridiumTeams.getMissionManager().handleMissionUpdate(team,
+                            event.getEntity().getLocation().getWorld(),
+                            "ENVIRONMENTAL_KILL",
+                            event.getEntityType().name(),
+                            1);
+                });
+            }
+            return;
+        }
+
+        if (killer != null) {
+            U user = iridiumTeams.getUserManager().getUser(killer);
+            iridiumTeams.getTeamManager().getTeamViaID(user.getTeamID()).ifPresent(team -> {
+                iridiumTeams.getMissionManager().handleMissionUpdate(team,
+                        killer.getLocation().getWorld(),
+                        "KILL",
+                        event.getEntityType().name(),
+                        1);
+            });
+        }
     }
-
 }
