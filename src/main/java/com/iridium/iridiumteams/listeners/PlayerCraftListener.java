6d582784de916e60ec7134f46ed352d1b2b0df78
Genre Mamanao <41022228+glenngenre@users.noreply.github.com>
2026-09-14 11:42:27 +0800

    Add CrafterCraftEvent

--- src/main/java/com/iridium/iridiumteams/listeners/PlayerCraftListener.java
+++ src/main/java/com/iridium/iridiumteams/listeners/PlayerCraftListener.java
@@ -1,4 +1,3 @@
-
 package com.iridium.iridiumteams.listeners;
 
 import com.cryptomorin.xseries.XMaterial;
@@ -6,16 +5,22 @@ import com.iridium.iridiumteams.IridiumTeams;
 import com.iridium.iridiumteams.database.IridiumUser;
 import com.iridium.iridiumteams.database.Team;
 import lombok.AllArgsConstructor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.CrafterCraftEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Optional;
 
 @AllArgsConstructor
 public class PlayerCraftListener<T extends Team, U extends IridiumUser<T>> implements Listener {
@@ -47,5 +52,52 @@ public class PlayerCraftListener<T extends Team, U extends IridiumUser<T>> imple
                 return;
             }
         }
+    }
+    
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onCrafterCraft(CrafterCraftEvent event) {
+        Location loc = event.getBlock().getLocation();
+
+        Optional<T> teamOpt = iridiumTeams.getTeamManager().getTeamViaLocation(loc);
+        if (teamOpt.isEmpty()) {
+            return;
+        }
+
+        T team = teamOpt.get();
+
+        ItemStack result = event.getResult();
+        if (result == null || result.getType().isAir()) {
+            return;
+        }
+
+        Recipe recipe = event.getRecipe();
+        if (recipe != null) {
+            if (recipe instanceof ShapedRecipe) {
+                for (ItemStack item : ((ShapedRecipe) recipe).getIngredientMap().values()) {
+                    if (item != null && iridiumTeams.getTeamManager().isBankItem(item)) {
+                        event.setCancelled(true);
+                        return;
+                    }
+                }
+            } else if (recipe instanceof ShapelessRecipe) {
+                for (ItemStack item : ((ShapelessRecipe) recipe).getIngredientList()) {
+                    if (item != null && iridiumTeams.getTeamManager().isBankItem(item)) {
+                        event.setCancelled(true);
+                        return;
+                    }
+                }
+            }
+        }
+
+        XMaterial material = XMaterial.matchXMaterial(result.getType());
+        int amount = result.getAmount();
+
+        iridiumTeams.getMissionManager().handleMissionUpdate(
+                team,
+                loc.getWorld(),
+                "CRAFT",
+                material.name(),
+                amount
+        );
     }
 }
